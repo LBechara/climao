11b1f3058d05689988de0a8edd91b9b811e7af87
LBechara <becharalucas@hotmail.com>
2022-04-18 01:34:51 -0300

    chamando api pelo input

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 // import {ErrorStateMatcher} from '@angular/material/core';
 
 
@@ -14,36 +16,37 @@ export class InputComponent implements OnInit {
   textForm!: FormGroup;
   apiKey: string = "4d8fb5b93d4af21d66a2948710284366";
   url: string = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+  nomeCidade: string = "";
+  temperaturaMinima: any;
+  temperaturaMaxima: any;
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.textForm = this.formBuilder.group({
-      cidade: ['cidade', Validators.required]
+      cidade: ['', Validators.required]
     })
     console.log(this.textForm)
-    this.getClima()
   }
 
   onSubmit() {
-    console.log(this.textForm)
+    console.log(this.textForm.value.cidade)
+    this.mostraRetorno(this.textForm.value.cidade)
   }
-
-  getClima() {
-    let params = new HttpParams({
-      fromObject: {
-        q: 'sao paulo',
-        appid: this.apiKey
-      }
-    });
-
-    return this.http.get<any>('https://api.openweathermap.org/data/2.5/weather', { params })
-      .toPromise()
-      .then( response => {
-
-        console.log(response)
-
-        }
-      )
+  
+  getClimao(cidadeProcurada: string) { 
+    return ajax({
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${cidadeProcurada}&appid=${this.apiKey}&units=metric`,
+      method: 'GET',
+    })
+  }
+    
+  mostraRetorno(cidade: string) {
+    this.getClimao(cidade).subscribe(el => {
+      console.log(el.response)
+      this.nomeCidade = el.response.name
+      this.temperaturaMinima = el.response.main.temp_min
+      this.temperaturaMaxima = el.response.main.temp_max
+    })
   }
 }
